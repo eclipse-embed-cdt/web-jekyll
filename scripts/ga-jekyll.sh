1f4dda2493d0d16a4e4e57bff5c995d402ef0327
Liviu Ionescu <ilg@livius.net>
2020-12-17 14:48:08 +0200

    jekyll.yml: call scripts/ga-jekyll.sh

---
+++ scripts/ga-jekyll.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+# Safety settings (see https://gist.github.com/ilg-ul/383869cbb01f61a51c4d).
+
+if [[ ! -z ${DEBUG} ]]
+then
+  set ${DEBUG} # Activate the expand mode if DEBUG is anything but empty.
+else
+  DEBUG=""
+fi
+
+set -o errexit # Exit if command failed.
+set -o pipefail # Exit if pipe failed.
+set -o nounset # Exit if variable not set.
+
+# Remove the initial space and instead use '\n'.
+IFS=$'\n\t'
+
+# -----------------------------------------------------------------------------
+
+# https://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables
+
+# -----------------------------------------------------------------------------
+
+function run_verbose()
+{
+  echo "[$@]"
+  "$@"
+}
+
+# -----------------------------------------------------------------------------
+
+pwd
+echo "env..."
+env | sort
+
+ruby --version
+bundler --version
+
+
+# -----------------------------------------------------------------------------
+
